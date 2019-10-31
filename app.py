c034d52686ce5049f16b882362d92ee7bcea3883
adonegan <48014273+adonegan@users.noreply.github.com>
2019-10-31 14:19:25 +0000

    changed os.getenv info

--- app.py
+++ app.py
@@ -9,7 +9,5 @@ def index():
     return "<h1>Hello There</h1>"
 
 
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', '5000'), debug=False)
