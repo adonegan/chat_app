9064f288414017bc2d2bbb1eb0ad218a70891cec
adonegan <48014273+adonegan@users.noreply.github.com>
2019-11-01 15:20:03 +0000

    updated to include form function

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
@@ -18,7 +19,14 @@ def get_all_messages():
 @app.route("/")
 def index():
     """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
+
 
 @app.route("/<username>")
 def user(username):
