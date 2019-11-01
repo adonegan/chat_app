6a7a5dc2f41deda89d85a75a67b447a81fd6fadb
adonegan <48014273+adonegan@users.noreply.github.com>
2019-11-01 17:00:21 +0000

    final check

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
+
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
@@ -13,15 +14,14 @@ def add_message(username, message):
     messages.append({"timestamp": now, "from": username, "message": message})
 
 
-
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
     if request.method == "POST":
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect(url_for("user", username = session["username"]))
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
@@ -29,14 +29,16 @@ def index():
 @app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
     """Add and display chat message"""
-
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_message(username, message)
         return redirect(url_for("user", username = session["username"]))
 
-    return render_template("chat.html", username = username, chat_messages = messages)
+    return render_template("chat.html", username = username, 
+    
+    chat_messages = messages)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '127.0.0.1'), port=os.getenv('PORT', '5000'), debug=False)
